e74936d2ed14b91fca2d4cfb87068825fdacb227
URMIL RUPARELIYA <urmil8989@gmail.com>
2022-12-13 03:43:41 -0800

    initial components added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./components/Header";
+import MainPage from "./components/Main";
+import Footer from "./components/Footer";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Profile from "./components/Profile";
+import "./App.css";
 
-function App() {
+import { Routes, Route } from "react-router-dom";
+const App = () =>
+{
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"> 
+      <Header />
+      <Routes>
+        <Route path="/" element={ <MainPage /> } />
+        <Route path="/register" element={ <Register /> } />
+        <Route path="/login" element={ <Login /> } />
+        <Route path="/profile" element={ <Profile /> } />
+        <Route path="/test" element={ <Profile /> } />
+      </Routes>
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
